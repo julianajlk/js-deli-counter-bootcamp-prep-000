13c4113dd87e86d582cfa229744bf18a0bd4e9ad
Juliana Kang <julianajlk@gmail.com>
2018-05-15 18:09:24 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -15,9 +15,9 @@ function nowServing(katzDeliLine) {
 };
 
 function currentLine(katzDeliLine) {
-  if (true) {
+  if (katzDeliLine.length === 0) {
     return "The line is currently empty."
   } else {
-    
+
   }
 };
