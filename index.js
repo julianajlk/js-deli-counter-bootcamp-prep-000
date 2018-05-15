11230981c98d9d6ed3aadc46227ebcdf56fc377f
Juliana Kang <julianajlk@gmail.com>
2018-05-15 17:59:40 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,13 +2,13 @@ var katzDeli = [];
 
 function takeANumber(katzDeliLine, name) {
   katzDeliLine.push(name);
-  return `Welcome, ${name}. You are number $(katzDeliLine.length) in line.`;
+  return `Welcome, ${name}. You are number ${katzDeliLine.length} in line.`;
 };
 
 function nowServing(katzDeliLine) {
   if (katzDeliLine.length === 0) {
     return "There is nobody waiting to be served!";
   } else {
-
+    
   }
 };
